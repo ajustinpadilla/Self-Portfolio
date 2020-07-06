96d3c2bfe25af30185b632e7faa1b6e581d7b757
Angel Padilla <Ajustinpadilla@gmail.com>
2020-07-06 17:20:45 -0600

    Add JavaScript

--- JavaScript/self-Portfolio.js
+++ JavaScript/self-Portfolio.js
@@ -1,7 +1,11 @@
 function validateForm() {
     let Name = document.forms["Contact"]["Name"].value;
     let Email = document.forms["Contact"]["Email"].value;
-    if (Name === "" || Name ==="Your Name") {
+    let closingForm = document.forms["Contact"]["closeFormBtn"].value;
+    if ( closingForm === "cancel") {
+        return false
+    }
+    else if (Name === "" || Name ==="Your Name") {
         alert("Please fill out your Name");
         return false;
     }
@@ -9,5 +13,17 @@ function validateForm() {
         alert("Please put an email so I may contact you");
         return false;
     }
+}
+
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+    document.getElementById("openFormBtn").style.display = "none";
+    document.getElementById("closeFormBtn").value = "open";
+}
+
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+    document.getElementById("openFormBtn").style.display = "block";
+    document.getElementById("closeFormBtn").value = "cancel";
 }
 
